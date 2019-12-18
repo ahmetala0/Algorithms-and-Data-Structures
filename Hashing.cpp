f7f8ce9b307827ca43debbe6d6b7d5e903f00a0d
Ahmet Ala <44473668+ahmetala0@users.noreply.github.com>
2019-12-18 22:24:09 +0300

    Hashing
    hashing

---
+++ Hashing.cpp
@@ -0,0 +1,40 @@
+// it gives the number of unique substrings of a string
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int mx=1e5+5;
+unsigned ll p[mx];
+int main(){
+	p[0]=1;
+	for(ll i=1;i<mx;++i){
+		p[i]=(p[i-1]*53);
+	}
+	
+	ll n,k;cin >> n >> k;
+	string s;cin >> s;
+	set <long long> st;
+	unsigned ll sum=0;
+	for(ll i=0;i<k;++i){
+		sum+=(p[k-i-1]*(s[i]));
+	}
+	st.insert(sum);
+	for(ll i=k;i<n;++i){
+		sum=(sum-(p[k-1]*(s[i-k])));
+		sum=(sum*53);
+		sum=(sum+s[i]);
+		st.insert(sum);
+	}
+	cout << st.size() << endl;
+}
+
+/*
+	~example~
+
+	input:
+	7 3
+	dogodog
+
+	output:
+	4
+
+*/
